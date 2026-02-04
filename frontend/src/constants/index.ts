6ffbb1a1ad2ea7f7ab49556e67d0412b0d9256f2
tienlevi <nguyentrachtien2401@gmail.com>
2026-02-04 14:52:28 +0700

    fix: run backend in deploy

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,2 +1,2 @@
-export const baseURL = "http://localhost:3000";
+export const baseURL = import.meta.env.VITE_BASE_URL || "http://localhost:3000";
 export const sofascoreURL = "https://www.sofascore.com/api/v1";
