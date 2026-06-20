7f4a56db1e5bfef0e3651f2a427effe9e19b076a
tienlevi <nguyentrachtien2401@gmail.com>
2026-06-20 23:22:48 +0700

    update: formatted season text

--- backend/src/services/leagues.js
+++ backend/src/services/leagues.js
@@ -1,8 +1,10 @@
 import { fotmobApi } from "../configs/axios.js";
 
 export const fetchLeague = async (season) => {
+  const formatSeasonText = encodeURIComponent(season.replace("-", "/")); // example: 2025-2026
+
   const response = await fotmobApi.get(
-    `/data/leagues?id=47&ccode3=VNM&season=${season}`,
+    `/data/leagues?id=47&ccode3=VNM&season=${formatSeasonText}`,
   );
   return response.data;
 };
