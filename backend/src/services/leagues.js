11563060b23bf642836b4c98152471d0f9a0986b
tienlevi <nguyentrachtien2401@gmail.com>
2026-06-22 20:39:14 +0700

    fix: query params in fetchLeague service

--- backend/src/services/leagues.js
+++ backend/src/services/leagues.js
@@ -5,8 +5,8 @@ export const fetchLeague = async (season) => {
     ? encodeURIComponent(season.replace("-", "/"))
     : ""; // example: 2025-2026
 
-  const response = await fotmobApi.get(`/data/leagues?id=47&ccode3=VNM`, {
-    params: { season: formatSeasonText },
-  });
+  const response = await fotmobApi.get(
+    `/data/leagues?id=47&ccode3=VNM${season && `&season=${formatSeasonText}`}`,
+  );
   return response.data;
 };
